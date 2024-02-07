0bb02d4f2bbeacc63f24bc8efa88a56f37553eb5
David Le <43181047+DavidVLe1@users.noreply.github.com>
2024-02-07 03:19:51 +0000

    changed file name, added controller, erb files, and routes for 2d10,1d20,5d4.

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -8,4 +8,21 @@ class ZebraController < ApplicationController
     @second_die = rand(1..6)
     render({ :template => "dice_templates/two_six" })
   end
+  def two_ten
+    @first_die = rand(1..10)
+    @second_die = rand(1..10)
+    render({ :template => "dice_templates/two_ten" })
+  end
+  def one_twenty
+    @first_die = rand(1..20)
+    render({ :template => "dice_templates/one_twenty" })
+  end
+  def five_four
+    @first_die = rand(1..4)
+    @second_die = rand(1..4)
+    @third_die = rand(1..4)
+    @fourth_die = rand(1..4)
+    @fifth_die = rand(1..4)
+    render({ :template => "dice_templates/five_four" })
+  end
 end
